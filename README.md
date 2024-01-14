89f397a4fc7fc73c5455310cdde5910b8a658719
Keith Oshima <koshima789@gmail.com>
2024-01-14 13:27:36 -0500

    Added output_folder arg. Updated docs.

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 This is a modified version of [HumAS-HMMER](https://github.com/enigene/HumAS-HMMER). Everything you need to produce ASat annotation standart pack (except [StV](https://github.com/fedorrik/stv) script) are in this repo.
 
 Usage: 
-```
-./hmmer-run.sh input_folder AS-HORs-hmmer3.3.2-120124.hmm number_of_threads
-./hmmer-run_SF.sh input_folder AS-SFs-hmmer3.0.290621.hmm number_of_threads
+```bash
+./hmmer-run.sh input_folder/ output_folder/ AS-HORs-hmmer3.3.2-120124.hmm number_of_threads
+./hmmer-run_SF.sh input_folder/ output_folder/ AS-SFs-hmmer3.0.290621.hmm number_of_threads
 ```
 
-RECOMMENDATION: If you have fasta file with few sequeces I recommend to split them into separate files with one sequence in each. So, script will proccess each sequence separately (in a loop) which will take less storage and time. 
+RECOMMENDATION: If you have fasta file with few sequences I recommend to split them into separate files with one sequence in each. So, script will proccess each sequence separately (in a loop) which will take less storage and time. 
 
 *In the first commit scripts hmmer-run.sh and hmmertblout2bed.awk were the same as in the [enigene](https://github.com/enigene?tab=repositories) repositories. After that, I added few changes (always adding comment which starts with # FEDOR). And I made two versions of hmmer-run.sh script.*
 
@@ -20,11 +20,17 @@ For each assembly we need to generate 4 bed files: AS-HOR, AS-HOR+SF, AS-strand,
  - AS-strand is obtained from AS-SF by one awk command which changes colors (hmmer-run_SF.sh line 63)
 
 So, to get files like in the test dir, run:
+    
+```bash
+./hmmer-run.sh test/ test/ AS-HORs-hmmer3.3.2-120124.hmm 8
+```
 
-    ./hmmer-run.sh test/ AS-HORs-hmmer3.3.2-120124.hmm 8
 Output beds: AS-HOR+SF, AS-HOR
 
-    ./hmmer-run_SF.sh test/ AS-SFs-hmmer3.0.290621.hmm 8
+```bash
+./hmmer-run_SF.sh test/ test/ AS-SFs-hmmer3.0.290621.hmm 8
+```
+
 Output beds: AS-SF, AS-strand
 
 ### LAST UPDATE 01/12/2024
--- hmmer-run.sh
+++ hmmer-run.sh
@@ -7,9 +7,9 @@ set -o errexit
 # Store working directory so script can be run outside of dir.
 wd=$(dirname $0)
 
-if [[ $# -ne 3 ]]
+if [[ $# -ne 4 ]]
 then
-    echo "usage: $0 input_folder path_and_name_of_the_profile number_of_threads" 1>&2
+    echo "usage: $0 input_folder output_folder path_and_name_of_the_profile number_of_threads" 1>&2
     exit 1
 fi
 
@@ -19,18 +19,25 @@ then
     exit 1
 fi
 
-if [ ! -s $2 ]
+if [ ! -d $2 ]
 then
-    echo "$2 file not found!" 1>&2
+    echo "$2 is not a directory!" 1>&2
     exit 1
 fi
 
-dir=$1
-hmm_profile_name=$2
-threads=$3
+if [ ! -s $3 ]
+then
+    echo "$3 file not found!" 1>&2
+    exit 1
+fi
+
+input_dir=$1
+output_dir=$2
+hmm_profile_name=$3
+threads=$4
 
 # FEDOR: I usually use .fa extension
-files=( $(find "$dir" -type f -name '*.fa' -print) )
+files=( $(find "$input_dir" -type f -name '*.fa' -print) )
 
 p="$(basename "$hmm_profile_name")"
 bp="${p%.*}"
@@ -43,35 +50,35 @@ do
     echo "$filename"
 
 #   HMMER analysis
-    nhmmer --cpu $threads --notextw --noali --tblout $wd/nhmmer-$bp-vs-$bn-tbl.out -o /dev/null $hmm_profile_name $filename
+    nhmmer --cpu $threads --notextw --noali --tblout $output_dir/nhmmer-$bp-vs-$bn-tbl.out -o /dev/null $hmm_profile_name $filename
 
 #   Converting HMM table output to the BED format with filtering by threshold score to length
 #   https://github.com/enigene/hmmertblout2bed
-    awk -v th=0.7 -f $wd/hmmertblout2bed.awk $wd/nhmmer-$bp-vs-$bn-tbl.out > $wd/nhmmer-$bp-vs-$bn-tbl.bed
+    awk -v th=0.7 -f $wd/hmmertblout2bed.awk $output_dir/nhmmer-$bp-vs-$bn-tbl.out > $output_dir/nhmmer-$bp-vs-$bn-tbl.bed
 
 #   FEDOR: remove huge .out file as soon as it's used
-    rm $wd/nhmmer-$bp-vs-$bn-tbl.out
+    rm $output_dir/nhmmer-$bp-vs-$bn-tbl.out
 
 #   Sorting by name and coordinates
 #   recommended running sort with option --temporary-directory=/path/to/another/local/physical/disk
-    sort -k 1.4,1 -k 2,2n $wd/nhmmer-$bp-vs-$bn-tbl.bed > $wd/_nhmmer-t0-$bn.bed
+    sort -k 1.4,1 -k 2,2n $output_dir/nhmmer-$bp-vs-$bn-tbl.bed > $output_dir/_nhmmer-t0-$bn.bed
 
 #   Filter by score in each region
-    bedmap --max-element --fraction-either 0.1 $wd/_nhmmer-t0-$bn.bed > $wd/_nhmmer-t1-$bn.bed
+    bedmap --max-element --fraction-either 0.1 $output_dir/_nhmmer-t0-$bn.bed > $output_dir/_nhmmer-t1-$bn.bed
 
 #   Filter unique elements
     # FEDOR: don't skip SF monomers
-    awk "{if(!(\$0 in a)){a[\$0]; print}}" $wd/_nhmmer-t1-$bn.bed > $wd/_nhmmer-t0-$bn.bed
+    awk "{if(!(\$0 in a)){a[\$0]; print}}" $output_dir/_nhmmer-t1-$bn.bed > $output_dir/_nhmmer-t0-$bn.bed
 
 #   FEDOR: addicional overlap filtering
-    python3 ${wd}/overlap_filter.py $wd/_nhmmer-t0-$bn.bed > $wd/AS-HOR+SF-vs-$bn.bed
+    python3 $wd/overlap_filter.py $output_dir/_nhmmer-t0-$bn.bed > $output_dir/AS-HOR+SF-vs-$bn.bed
 
 #   FEDOR: AS-HOR only (skip SF monomers)
-    awk '{ if (length($4)==2) {next} print}' $wd/AS-HOR+SF-vs-$bn.bed > $wd/AS-HOR-vs-$bn.bed
+    awk '{ if (length($4)==2) {next} print}' $output_dir/AS-HOR+SF-vs-$bn.bed > $output_dir/AS-HOR-vs-$bn.bed
 
 #   Delete temporary files
-    rm $wd/_nhmmer-t1-$bn.bed
-    rm $wd/_nhmmer-t0-$bn.bed
-    rm $wd/nhmmer-$bp-vs-$bn-tbl.bed
+    rm $output_dir/_nhmmer-t1-$bn.bed
+    rm $output_dir/_nhmmer-t0-$bn.bed
+    rm $output_dir/nhmmer-$bp-vs-$bn-tbl.bed
 
 done
