cf78c7b3a919088b55346d2c4d9842fd75ddaf96
Keith Oshima <koshima789@gmail.com>
2024-01-14 12:24:52 -0500

    Updated scripts to allow running outside of project directory.

--- hmmer-run.sh
+++ hmmer-run.sh
@@ -3,6 +3,10 @@
 set -o pipefail
 set -o errexit
 
+
+# Store working directory so script can be run outside of dir.
+wd=$(dirname $0)
+
 if [[ $# -ne 3 ]]
 then
     echo "usage: $0 input_folder path_and_name_of_the_profile number_of_threads" 1>&2
@@ -39,35 +43,35 @@ do
     echo "$filename"
 
 #   HMMER analysis
-    nhmmer --cpu $threads --notextw --noali --tblout nhmmer-$bp-vs-$bn-tbl.out -o /dev/null $hmm_profile_name $filename
+    nhmmer --cpu $threads --notextw --noali --tblout $wd/nhmmer-$bp-vs-$bn-tbl.out -o /dev/null $hmm_profile_name $filename
 
 #   Converting HMM table output to the BED format with filtering by threshold score to length
 #   https://github.com/enigene/hmmertblout2bed
-    awk -v th=0.7 -f hmmertblout2bed.awk nhmmer-$bp-vs-$bn-tbl.out > nhmmer-$bp-vs-$bn-tbl.bed
+    awk -v th=0.7 -f $wd/hmmertblout2bed.awk $wd/nhmmer-$bp-vs-$bn-tbl.out > $wd/nhmmer-$bp-vs-$bn-tbl.bed
 
 #   FEDOR: remove huge .out file as soon as it's used
-    rm nhmmer-$bp-vs-$bn-tbl.out
+    rm $wd/nhmmer-$bp-vs-$bn-tbl.out
 
 #   Sorting by name and coordinates
 #   recommended running sort with option --temporary-directory=/path/to/another/local/physical/disk
-    sort -k 1.4,1 -k 2,2n nhmmer-$bp-vs-$bn-tbl.bed > _nhmmer-t0-$bn.bed
+    sort -k 1.4,1 -k 2,2n $wd/nhmmer-$bp-vs-$bn-tbl.bed > $wd/_nhmmer-t0-$bn.bed
 
 #   Filter by score in each region
-    bedmap --max-element --fraction-either 0.1 _nhmmer-t0-$bn.bed > _nhmmer-t1-$bn.bed
+    bedmap --max-element --fraction-either 0.1 $wd/_nhmmer-t0-$bn.bed > $wd/_nhmmer-t1-$bn.bed
 
 #   Filter unique elements
     # FEDOR: don't skip SF monomers
-    awk "{if(!(\$0 in a)){a[\$0]; print}}" _nhmmer-t1-$bn.bed > _nhmmer-t0-$bn.bed
+    awk "{if(!(\$0 in a)){a[\$0]; print}}" $wd/_nhmmer-t1-$bn.bed > $wd/_nhmmer-t0-$bn.bed
 
 #   FEDOR: addicional overlap filtering
-    python3 overlap_filter.py _nhmmer-t0-$bn.bed > AS-HOR+SF-vs-$bn.bed
+    python3 ${wd}/overlap_filter.py $wd/_nhmmer-t0-$bn.bed > $wd/AS-HOR+SF-vs-$bn.bed
 
 #   FEDOR: AS-HOR only (skip SF monomers)
-    awk '{ if (length($4)==2) {next} print}' AS-HOR+SF-vs-$bn.bed > AS-HOR-vs-$bn.bed
+    awk '{ if (length($4)==2) {next} print}' $wd/AS-HOR+SF-vs-$bn.bed > $wd/AS-HOR-vs-$bn.bed
 
 #   Delete temporary files
-    rm _nhmmer-t1-$bn.bed
-    rm _nhmmer-t0-$bn.bed
-    rm nhmmer-$bp-vs-$bn-tbl.bed
+    rm $wd/_nhmmer-t1-$bn.bed
+    rm $wd/_nhmmer-t0-$bn.bed
+    rm $wd/nhmmer-$bp-vs-$bn-tbl.bed
 
 done
--- hmmer-run_SF.sh
+++ hmmer-run_SF.sh
@@ -3,6 +3,9 @@
 set -o pipefail
 set -o errexit
 
+# Store working directory so script can be run outside of dir.
+wd=$(dirname $0)
+
 if [[ $# -ne 3 ]]
 then
     echo "usage: $0 input_folder path_and_name_of_the_profile number_of_threads" 1>&2
@@ -39,35 +42,35 @@ do
     echo "$filename"
 
 #   HMMER analysis
-    nhmmer --cpu $threads --notextw --noali --tblout nhmmer-$bp-vs-$bn-tbl.out -o /dev/null $hmm_profile_name $filename
+    nhmmer --cpu $threads --notextw --noali --tblout $wd/nhmmer-$bp-vs-$bn-tbl.out -o /dev/null $hmm_profile_name $filename
 
 #   Converting HMM table output to the BED format with filtering by threshold score to length
 #   https://github.com/enigene/hmmertblout2bed
-    awk -v th=0.7 -f hmmertblout2bed.awk nhmmer-$bp-vs-$bn-tbl.out > nhmmer-$bp-vs-$bn-tbl.bed
+    awk -v th=0.7 -f $wd/hmmertblout2bed.awk $wd/nhmmer-$bp-vs-$bn-tbl.out > $wd/nhmmer-$bp-vs-$bn-tbl.bed
 
 #   FEDOR: remove huge .out file as soon as it's used
-    rm nhmmer-$bp-vs-$bn-tbl.out
+    rm $wd/nhmmer-$bp-vs-$bn-tbl.out
 
 #   Sorting by name and coordinates
 #   recommended running sort with option --temporary-directory=/path/to/another/local/physical/disk
-    sort -k 1.4,1 -k 2,2n nhmmer-$bp-vs-$bn-tbl.bed > _nhmmer-t0-$bn.bed
+    sort -k 1.4,1 -k 2,2n $wd/nhmmer-$bp-vs-$bn-tbl.bed > $wd/_nhmmer-t0-$bn.bed
 
 #   Filter by score in each region
-    bedmap --max-element --fraction-either 0.1 _nhmmer-t0-$bn.bed > _nhmmer-t1-$bn.bed
+    bedmap --max-element --fraction-either 0.1 $wd/_nhmmer-t0-$bn.bed > $wd/_nhmmer-t1-$bn.bed
 
 #   Filter unique elements
     # FEDOR: don't skip SF monomers
-    awk "{if(!(\$0 in a)){a[\$0]; print}}" _nhmmer-t1-$bn.bed > _nhmmer-t0-$bn.bed
+    awk "{if(!(\$0 in a)){a[\$0]; print}}" $wd/_nhmmer-t1-$bn.bed > $wd/_nhmmer-t0-$bn.bed
     
 #   FEDOR: addicional overlap filtering
-    python3 overlap_filter.py _nhmmer-t0-$bn.bed > AS-SF-vs-$bn.bed
+    python3 $wd/overlap_filter.py $wd/_nhmmer-t0-$bn.bed > $wd/AS-SF-vs-$bn.bed
 
 #   FEDOR: AS-strand annotation. "+" is blue, "-" is red
-    awk -F $'\t' 'BEGIN {OFS = FS} {if ($6=="+") {$9="0,0,255"}; if ($6=="-") {$9="255,0,0"} print $0}' AS-SF-vs-$bn.bed > AS-strand-vs-$bn.bed
+    awk -F $'\t' 'BEGIN {OFS = FS} {if ($6=="+") {$9="0,0,255"}; if ($6=="-") {$9="255,0,0"} print $0}' $wd/AS-SF-vs-$bn.bed > $wd/AS-strand-vs-$bn.bed
 
 #   Delete temporary files
-    rm _nhmmer-t1-$bn.bed
-    rm _nhmmer-t0-$bn.bed
-    rm nhmmer-$bp-vs-$bn-tbl.bed
+    rm $wd/_nhmmer-t1-$bn.bed
+    rm $wd/_nhmmer-t0-$bn.bed
+    rm $wd/nhmmer-$bp-vs-$bn-tbl.bed
 
 done
